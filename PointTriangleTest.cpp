304977c000ce4e2539cc2f3f81c6657726308d14
Christopher Dyken <cdyk@users.noreply.github.com>
2018-04-14 17:33:52 +0200

    Cleaned up math, added description of algo, color coded lines

--- PointTriangleTest.cpp
+++ PointTriangleTest.cpp
@@ -3,67 +3,28 @@
 #include <iostream>
 #include <fstream>
 #include <cassert>
+#include <string>
 
 //N[0] = u[1]v[2] - u[2]v[1];
 //N[1] = u[2]v[0] - u[0]v[2];
 //N[2] = u[0]v[1] - u[1]v[0];
 
-void nearestPointOnTriangle(float(&Q)[3], const float(&A)[3], const float(&B)[3], const float(&C)[3], const float(&P)[3])
+unsigned nearestPointOnTriangle(float(&Q)[3], const float(&A)[3], const float(&B)[3], const float(&C)[3], const float(&P)[3])
 {
-  Q[0] = 0.f;
-  Q[1] = 0.f;
-  Q[2] = 0.f;
-
-
-  float PA[3], PB[3], PC[3], AB[3], AC[3], N[3];
-  float PAxPB[3];
-  float PBxPC[3];
-  float PCxPA[3];
-
-  for (unsigned i = 0; i < 3; i++) {
-    PA[i] = A[i] - P[i];
-    PB[i] = B[i] - P[i];
-    PC[i] = C[i] - P[i];
-
-    AB[i] = B[i] - A[i];
-    AC[i] = C[i] - A[i];
-  }
-
-  // Calc barycentric coords.
-  N[0] = AB[1]*AC[2] - AB[2]*AC[1];
-  N[1] = AB[2]*AC[0] - AB[0]*AC[2];
-  N[2] = AB[0]*AC[1] - AB[1]*AC[0];
-
-  PAxPB[0] = PA[1] * PB[2] - PA[2] * PB[1];
-  PAxPB[1] = PA[2] * PB[0] - PA[0] * PB[2];
-  PAxPB[2] = PA[0] * PB[1] - PA[1] * PB[0];
-  float w_ab = N[0] * PAxPB[0] + N[1] * PAxPB[1] + N[2] * PAxPB[2];
-
-  PBxPC[0] = PB[1] * PC[2] - PB[2] * PC[1];
-  PBxPC[1] = PB[2] * PC[0] - PB[0] * PC[2];
-  PBxPC[2] = PB[0] * PC[1] - PB[1] * PC[0];
-  float w_bc = N[0] * PBxPC[0] + N[1] * PBxPC[1] + N[2] * PBxPC[2];
-
-  PCxPA[0] = PC[1] * PA[2] - PC[2] * PA[1];
-  PCxPA[1] = PC[2] * PA[0] - PC[0] * PA[2];
-  PCxPA[2] = PC[0] * PA[1] - PC[1] * PA[0];
-  float w_ca = N[0] * PCxPA[0] + N[1] * PCxPA[1] + N[2] * PCxPA[2];
-
-  float w = w_ab + w_bc + w_ca;
-  assert(0 <= w);
-
-  uint32_t region = (w_ca < 0.f ? 4 : 0) | (w_bc < 0.f ? 2 : 0) | (w_ab < 0.f ? 1 : 0);
-  //
-  // Region encodes which of the vertices'/edge's Voronoi-region P is in:
+  // Algorithm:
+  // - For each edge, form an outwards plane NxAB, NxBC, and NxCA.
+  // - Distance from these planes, e.g. <AP,NxAB> forms barycentric coords when they are normalized.
+  // - These half-planes also forms the boundary of the Voronoi-cells of the vertices and edges of the triangle.
+  // - By inspecting the sign of the plane distances, we can determine which region P is in:
   // 
-  //       \110=6 |
-  //        \CA<0 |
-  //         \BC<0|
-  //          \   |
-  //           \  |
-  //            \ |
-  //             \|
-  //              C
+  //        \110=6 |
+  //         \CA<0 |
+  //          \BC<0|
+  //           \   |
+  //            \  |
+  //             \ |
+  //              \|
+  //               C
   //          CA<0 |\   BC<0
   //         100=4 | \  010=2
   //               |  \
@@ -72,29 +33,120 @@ void nearestPointOnTriangle(float(&Q)[3], const float(&A)[3], const float(&B)[3]
   //          CA<0 |     \ BC<0
   //         101=5 | 001=1\ 011=3
   //
+  // - If all distances are positive, the closest point is inside the triangle,
+  //   and we use the barycentric coords to find the position.
+  // - If one distance is positive and two negative, the closest point is a
+  //   corner, and we use that position.
+  // - If two distances are positive and one negative, the closest point is on
+  //   an edge. If on edge AB, the ratio of <AP,AB> and <PB,AB> (P orthognoally
+  //   projected onto AB) gives the position.
+
+  float AP[3];
+  float BP[3];
+  float CP[3];
+  float N[3];
+  float AB[3];
+  float BC[3];
+  float CA[3];
+  for (unsigned i = 0; i < 3; i++) {
+    AP[i] = P[i] - A[i];
+    BP[i] = P[i] - B[i];
+    CP[i] = P[i] - C[i];
+
+    AB[i] = B[i] - A[i];
+    BC[i] = C[i] - B[i];
+    CA[i] = A[i] - C[i];
+  }
+
+  // Calc barycentric coords.
+  N[0] = AB[1] * BC[2] - AB[2] * BC[1];
+  N[1] = AB[2] * BC[0] - AB[0] * BC[2];
+  N[2] = AB[0] * BC[1] - AB[1] * BC[0];
+
+  float NxAB[3];
+  NxAB[0] = N[1] * AB[2] - N[2] * AB[1];
+  NxAB[1] = N[2] * AB[0] - N[0] * AB[2];
+  NxAB[2] = N[0] * AB[1] - N[1] * AB[0];
+  float AP_dot_NxAB =
+    AP[0] * NxAB[0] +
+    AP[1] * NxAB[1] +
+    AP[2] * NxAB[2];
+
+  float NxBC[3];
+  NxBC[0] = N[1] * BC[2] - N[2] * BC[1];
+  NxBC[1] = N[2] * BC[0] - N[0] * BC[2];
+  NxBC[2] = N[0] * BC[1] - N[1] * BC[0];
+  float BP_dot_NxBC =
+    BP[0] * NxBC[0] +
+    BP[1] * NxBC[1] +
+    BP[2] * NxBC[2];
+
+  float NxCA[3];
+  NxCA[0] = N[1] * CA[2] - N[2] * CA[1];
+  NxCA[1] = N[2] * CA[0] - N[0] * CA[2];
+  NxCA[2] = N[0] * CA[1] - N[1] * CA[0];
+  float CP_dot_NxCA =
+    CP[0] * NxCA[0] +
+    CP[1] * NxCA[1] +
+    CP[2] * NxCA[2];
+
+
+  uint32_t region = (CP_dot_NxCA < 0.f ? 4 : 0) | (BP_dot_NxBC < 0.f ? 2 : 0) | (AP_dot_NxAB < 0.f ? 1 : 0);
+  float w_a = 0.f;
+  float w_b = 0.f;
+  float w_c = 0.f;
+
+  float(*e0)[3] = nullptr;
+  float(*e1)[3] = nullptr;
+
   switch (region)
   {
   case 0:   // 000 - inside ab, bc, ca
+    w_a = BP_dot_NxBC;
+    w_b = CP_dot_NxCA;
+    w_c = AP_dot_NxAB;
     break;
-  case 1:   // 001 - outside ab; inside bc, ca;
-    break;
-  case 2:   // 010 - outside bc; inside ab, ca;
-    break;
+   
   case 3:   // 011 - outside ab, bc; inside ca;
-    break;
-  case 4:   // 100 - outside ca; inside bc, ab;
+    w_b = 1.f;
     break;
   case 5:   // 101 - outside ca, ab; inside bc;
+    w_a = 1.f;
     break;
   case 6:   // 110 - outside ca, bc; inside ab
+    w_c = 1.f;
     break;
+
+  case 1:   // 001 - outside ab; inside bc, ca;
+    w_a = 1.f;
+    w_b = 1.f;
+    break;
+  case 2:   // 010 - outside bc; inside ab, ca;
+    w_b = 1.f;
+    w_c = 1.f;
+    break;
+  case 4:   // 100 - outside ca; inside bc, ab;
+    w_c = -(AP[0] * CA[0] + AP[1] * CA[1] + AP[2] * CA[2]);
+    w_a = CP[0] * CA[0] + CP[1] * CA[1] + CP[2] * CA[2];
+    break;
+
   case 7:   // 111 - outside ca, bc, ab
+    w_a = w_b = w_c = 1.f;
     break;
   default:
     break;
   }
 
+  w_a = w_a < 0.f ? 0.f : w_a;
+  w_b = w_b < 0.f ? 0.f : w_b;
+  w_c = w_c < 0.f ? 0.f : w_c;
 
+  float s = 1.f/(w_a + w_b + w_c);
+  for (unsigned i = 0; i < 3; i++) {
+    Q[i] = s * (w_a * A[i] + w_b * B[i] + w_c * C[i]);
+  }
+
+  return region;
 }
 
 
@@ -109,7 +161,17 @@ int main()
   const unsigned m = 10;
 
 
+  std::ofstream mat("dump.mtl");
+  for (unsigned i = 0; i < 8; i++) {
+    mat << "newmtl m" << i << "\n";
+    mat << "Kd "
+      << (i & 1 ? 1 : 0) << ' '
+      << (i & 2 ? 1 : 0) << ' '
+      << (i & 4 ? 1 : 0) << '\n';
+  }
+
   std::ofstream out("dump.obj");
+  out << "mtllib dump.mtl\n";
 
   for (unsigned j = 0; j < m; j++) {
     for (unsigned i = 0; i < n; i++) {
@@ -122,7 +184,8 @@ int main()
         r * std::cos(theta)
       };
       float Q[3];
-      nearestPointOnTriangle(Q, A, B, C, P);
+      auto region = nearestPointOnTriangle(Q, A, B, C, P);
+      out << "usemtl m" << ((region + 1) & 7) << "\n";
 
       out << "v " << Q[0] << ' ' << Q[1] << ' ' << Q[2] << '\n';
       out << "v " << P[0] << ' ' << P[1] << ' ' << P[2] << '\n';
@@ -132,6 +195,7 @@ int main()
   }
 
   // output triangle
+  out << "usemtl m0\n";
   out << "v " << A[0] << ' ' << A[1] << ' ' << A[2] << '\n';
   out << "v " << B[0] << ' ' << B[1] << ' ' << B[2] << '\n';
   out << "v " << C[0] << ' ' << C[1] << ' ' << C[2] << '\n';
